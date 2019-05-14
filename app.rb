b0f74418f91ffd40cebf1049af873824295ca2b2
Hanna Engel <hengel@alumni.berklee.edu>
2019-05-14 14:34:22 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer'
+require 'byebug'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @piglatinizer = Piglatinizer.new(params[:user_input])
+    erb :results
+  end
+
 end
